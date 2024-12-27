beb4ba9f0c9759fe40f9c20a0fc06e138290350b
zoesheare <zoeshare@hotmail.com>
2024-12-27 11:06:22 +0800

    feat: 添加网站图标和元数据

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp; 
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>云裳茶江 - 恭城瑶乡数字展示</title>
+        <meta name="description" content="探索恭城瑶乡的数字未来" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+} 
